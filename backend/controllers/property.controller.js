8bccb6bea9dc5b7de6fa3ddcf462d91aad3ac95d
gogi999 <g.petrovic9@gmail.com>
2023-02-16 19:32:49 +0100

    Added user and property controllers, models and routes

---
+++ backend/controllers/property.controller.js
@@ -0,0 +1,120 @@
+import Property from '../models/property.model.js';
+
+export const getAllProperties = async (req, res) => {
+    try {
+        const properties = await Property.find({});
+
+        return res.status(200).json(properties);
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+export const getFeatured = async (req, res) => {
+    try {
+        const featuredProperties = await Property
+            .find({ featured: true })
+            .populate('currentOwner', '-password');
+
+        return res.status(200).json(featuredProperties);
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+export const getAllFromSpecificType = async (req, res) => {
+    const type = req.query;
+
+    try {
+        if (type) {
+            const properties = await Property
+                .find(type)
+                .populate('currentOwner', '-password');
+
+            return res.status(200).json(properties);
+        } else {
+            res.status(404).json({ msg: 'No such type of property found!' });
+        }
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+export const getCountOfTypes = async (req, res) => {
+    try {
+        const beachType = await Property.countDocuments({ type: 'beach' });
+        const mountainType = await Property.countDocuments({ type: 'mountain' });
+        const villageType = await Property.countDocuments({ type: 'village' });
+
+        return res.status(200).json({
+            beach: beachType,
+            mountain: mountainType,
+            village: villageType,
+        });
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+export const getProperty = async (req, res) => {
+    try {
+        const property = await Property.find(req.params.id).populate('currentOwner', '-password');
+
+        if (!property) {
+            throw new Error('No such property with that id!');
+        } else {
+            return res.status(200).json(property);
+        }
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+export const createProperty = async (req, res) => {
+    try {
+        const newProperty = await Property.create({
+            ...req.body,
+            currentOwner: req.user.id
+        });
+
+        return res.status(201).json(newProperty);
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+export const updateProperty = async (req, res) => {
+    try {
+        const property = await Property.findById(req.params.id);
+
+        if (property.currentOwner !== req.user.id) {
+            throw new Error('You are not allowed to update this property!');
+        } else {
+            const updatedProperty = await Property.findByIdAndUpdate(
+                req.params.id, 
+                { $set: req.body },
+                { new: true }
+            );
+
+            return res.status(200).json(updatedProperty);
+        }
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+export const deleteProperty = async (req, res) => {
+    try {
+        const property = await Property.findById(req.params.id);
+
+        if (property.currentOwner !== req.user.id) {
+            throw new Error('You are not allowed to delete this property!');
+        } else {
+            await Property.delete();
+            
+            return res.status(200).json({ msg: 'Property successfully deleted!' });
+        }
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
--- backend/routes/property.routes.js
+++ backend/routes/property.routes.js
@@ -1,5 +1,26 @@
 import express from 'express';
 
+import {
+  createProperty,
+  deleteProperty,
+  getAllFromSpecificType,
+  getAllProperties,
+  getCountOfTypes,
+  getFeatured,
+  getProperty,
+  updateProperty,
+} from '../controllers/property.controller.js';
+import verifyToken from '../middlewares/verifyToken.js';
+
 const router = express.Router();
+
+router.get('/getAll', getAllProperties);
+router.get('/find/featured', getFeatured);
+router.get('/find', getAllFromSpecificType);
+router.get('/find/types', getCountOfTypes);
+router.get('/find/:id', getProperty);
+router.post('/', verifyToken, createProperty);
+router.put('/:id', verifyToken, updateProperty);
+router.put('/:id', verifyToken, deleteProperty);
 
 export default router;
